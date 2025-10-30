6d0acd189171ca5849802747e6c356454f8848bf
kavindasj-at-832266882009 <kavindasj@cf.lk>
2025-10-30 16:54:10 +0530

    up

--- src/app/modules/teacher/components/teacher-dashboard/teacher-dashboard.component.ts
+++ src/app/modules/teacher/components/teacher-dashboard/teacher-dashboard.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-teacher-dashboard',
-  // standalone: true,
-  // imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './teacher-dashboard.component.html',
   styleUrls: ['./teacher-dashboard.component.scss']
 })
 export class TeacherDashboardComponent {
+  isModalOpen = false;
 
+  openModal() {
+    console.log('Button clicked!'); // ✅ To verify click works
+    this.isModalOpen = true;
+  }
+
+  closeModal() {
+    this.isModalOpen = false;
+  }
 }
